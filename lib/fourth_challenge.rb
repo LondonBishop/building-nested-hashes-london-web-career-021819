74e88a0103592a3afcec75070d0978ecba0b0a5d
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-13 14:20:38 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,29 +1,29 @@
 def fourth_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {:name=>"Romeo", :age=>"15", :status=>"alive"},
+      :patriarch => {:name =>"Lord Montague", :age =>"53"},
+      :matriarch => {:name => "Lady Montague", :age=> "54"},
+      :hero => {:name =>"Romeo", :age =>"15", :status =>"alive"},
       :hero_friends => [
         {
-          :name=>"Benvolio",
-          :age=>"17",
-          :attidute=>"worried"
+          :name =>"Benvolio",
+          :age =>"17",
+          :attidute =>"worried"
         } ,
         {
-          :name=>"Mercutio",
-          :age=>"18",
-          :attidute=>"hot-headed"
+          :name =>"Mercutio",
+          :age =>"18",
+          :attidute =>"hot-headed"
         }#your hashes here!
       ]
    },
    :capulet => {
-      :patriarch => {:name=>"Lord Capulet", :age: "50"},
-      :matriarch => {:name "Lady Capulet", :age: "51"},
-      :heroine => {:name "Juliet", :age: "15", :status: "alive"},
+      :patriarch => {:name =>"Lord Capulet", :age =>"50"},
+      :matriarch => {:name "Lady Capulet", :age => "51"},
+      :heroine => {:name "Juliet", :age =>"15", :status => "alive"},
       :heroine_friends => [
-        {:name =>"Steven", :age=>"30", :attidute=>"confused"},
-        {:name =>"Nurse", :age=>"44", :attidute=>"worried"}
+        {:name =>"Steven", :age =>"30", :attidute =>"confused"},
+        {:name =>"Nurse", :age =>"44", :attidute =>"worried"}
         #your hashes here!
       ]
    }
